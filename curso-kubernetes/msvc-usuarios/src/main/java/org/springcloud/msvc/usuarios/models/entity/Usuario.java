a77ec2471d4ceadcb85b8c4ef72042bb6140fb63
David Forero <davidforero717@gmail.com>
2024-04-13 14:27:32 -0500

    Restontroller y metodos handler POST, PUT y DELETE

--- curso-kubernetes/msvc-usuarios/src/main/java/org/springcloud/msvc/usuarios/models/entity/Usuario.java
+++ curso-kubernetes/msvc-usuarios/src/main/java/org/springcloud/msvc/usuarios/models/entity/Usuario.java
@@ -9,12 +9,9 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-
     private String nombre;
-
     @Column(unique = true)
     private String email;
-
     private String password;
 
     public Long getId() {
@@ -23,6 +20,30 @@ public class Usuario {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
