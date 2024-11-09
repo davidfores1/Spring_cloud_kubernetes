0f23edb18c009b925fb782391181af536e548a15
David Forero <davidforero717@gmail.com>
2024-11-08 23:22:30 -0500

    Se crea nuevamente el modulo de customer

--- curso-kubernetes/msvc-usuarios/src/main/java/org/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package org.springcloud.msvc.usuarios.repositories;
-
-import org.springcloud.msvc.usuarios.models.entity.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-}
