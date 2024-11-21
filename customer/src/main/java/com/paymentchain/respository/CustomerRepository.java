8f20f9d6656d378800782e1dcb7d67592805a6c3
David Forero <davidforero717@gmail.com>
2024-11-20 22:39:28 -0500

    se realiza integracion de la libreria swagger

--- customer/src/main/java/com/paymentchain/respository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.paymentchain.respository;
-
-import com.paymentchain.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
