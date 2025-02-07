a052dd4276a48e53c728f4a9ed602a61ca7981dc
David Forero <davidforero717@gmail.com>
2025-02-06 22:26:22 -0500

    27. Crear la relación entre un cliente y muchos productos con JPA (CustomerProduct)

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
@@ -4,10 +4,13 @@
  */
 package com.paymentchain.customer.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 
 
@@ -23,8 +26,15 @@ public class Customer {
    @GeneratedValue(strategy = GenerationType.AUTO)  
    @Id
    private long id;
+   private String code;
+   private String iban;
    private String name;
    private String phone; 
+   private String address; 
+   
+   @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+   private CustomerProduct products;
+   
    
    
 }
---
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/entities/CustomerProduct.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paymentchain.customer.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
+import lombok.Data;
+
+/**
+ *
+ * @author David
+ */
+
+@Data
+@Entity        
+class CustomerProduct {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private long productId;
+    
+    @Transient
+    private String productName;
+    
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Customer.class)
+    
+    @JoinColumn(name = "customerId", nullable = true)
+    private Customer Customer;
+    
+}
