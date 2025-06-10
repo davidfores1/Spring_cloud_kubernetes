9296798ef1a8a5a5fdcbad84baef010279e9dc71
David Forero <davidforero717@gmail.com>
2025-06-09 22:01:44 -0500

    55. Crear un servidor de observabilidad con Spring boot y Spring Admin

---
+++ paymentchainparent/infraestructuradomain/springBootAdmin/src/main/java/com/paymentchain/springBootAdmin/SpringBootAdminApplication.java
@@ -0,0 +1,19 @@
+package com.paymentchain.springBootAdmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
